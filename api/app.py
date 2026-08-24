c04b39781961360b3cb0f4671704b92e6fe52f32
jke <kalebadagbe@gmail.com>
2026-08-24 11:00:02 +0100

    Delete api directory

--- api/app.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# Permet d'importer app.py qui se trouve à la racine du projet,
-# un dossier au-dessus de ce fichier (api/app.py).
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
